81c544e85167c8e234b2a37ee7bd0e44efc63063
Emily <ecmoeller@wisc.edu>
2019-05-04 17:13:54 -0500

    Read in first file

--- hw3.py
+++ hw3.py
@@ -9,3 +9,33 @@ import plotly.graph_objs as go
 
 # Create random data with numpy
 import numpy as np
+
+class Pair:
+    def __init__(self, proteinA, proteinB):
+        self.proteinA = proteinA
+        self.proteinB = proteinB  
+
+
+def main():
+    print("In main")
+    #Reading in data from txt file
+    with open('Human_PPI.csv','r') as f:
+        data = f.readlines()
+
+
+    count = 0
+    pairs = []
+    for line in data:
+        line = line.strip()
+        if (count == 0):
+            print("did nothing")
+        if(count >= 1 ):
+            pairterms = line.split('\t')
+            pair = Pair(pairterms[0], pairterms[1])
+            pairs.append(pair)
+
+        count += 1
+
+
+if __name__ == '__main__':
+    main()
