f374038bf8bfa4e577d7a91520a17d0ec1fd1c44
Emily <ecmoeller@wisc.edu>
2019-05-04 16:40:22 -0500

    Added first files

---
+++ hw3.py
@@ -0,0 +1,11 @@
+import math
+from operator import itemgetter 
+
+import plotly
+plotly.tools.set_credentials_file(username='moell162', api_key='XWeDQVjHzfvEN01WsE7p')
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Create random data with numpy
+import numpy as np
